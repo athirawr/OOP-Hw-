95e7bdf45353efbaf5ad867baab2612ca9bf6867
athirawr <126443165+athirawr@users.noreply.github.com>
2023-03-06 11:29:09 +0800

    Create SchoolList.java

---
+++ SchoolList.java
@@ -0,0 +1,17 @@
+public class SchoolList{
+    
+    //data section
+    
+    private School schools[] = new School[10];
+    private int currnum;
+    
+    //operations 
+    
+    public void addSchool(School sc){
+        
+        schools[currnum]= sc;
+        currnum++;
+        
+    }
+    
+}
