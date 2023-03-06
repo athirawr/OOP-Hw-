5fe3e3b6f3f91d92e67b3b431f951dddc3734981
athirawr <126443165+athirawr@users.noreply.github.com>
2023-03-06 11:29:49 +0800

    Create DSchoolList.java

---
+++ DSchoolList.java
@@ -0,0 +1,19 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+
+public class DSchoolList{
+    
+    ArrayList<School> schools = new ArrayList<>();
+    
+    public void addSchool(School sc){
+        
+        schools.add(sc);
+    }
+    
+    public void display() {
+        for (int i=0; i<schools.size(); i++){
+            System.out.print(schools.get(i));
+        }
+    }
+}
